14328fd6600d886bee99c9fb85664fea9b54c493
KULDIP MOCHI <53702326+kulu123-z@users.noreply.github.com>
2020-05-04 21:34:29 +0530

    Create LongestConsecutive1's.cpp

---
+++ LongestConsecutive1's.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int long1s(int n){
+  int ans=0;
+  while(x)
+  {
+    x=x&(x<<1);
+    ans++;
+  } 
+  return ans;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  cout<<long1s(n);
+}
